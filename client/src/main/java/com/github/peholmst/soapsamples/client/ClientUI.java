d98af7a197b298f443f362928185e1616eca70d2
Petter Holmström <petter@vaadin.com>
2014-04-16 23:27:57 +0300

    Removed unused imports

--- client/src/main/java/com/github/peholmst/soapsamples/client/ClientUI.java
+++ client/src/main/java/com/github/peholmst/soapsamples/client/ClientUI.java
@@ -21,8 +21,6 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
 
 public class ClientUI extends UI {
