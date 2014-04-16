b7cdeabdc3f4fdbcc7fd223580715479298910a7
Petter Holmström <petter@vaadin.com>
2014-04-16 22:54:11 +0300

    Added window for editing contacts

--- client/src/main/java/com/github/peholmst/soapsamples/client/ClientUI.java
+++ client/src/main/java/com/github/peholmst/soapsamples/client/ClientUI.java
@@ -1,17 +1,28 @@
 package com.github.peholmst.soapsamples.client;
 
 import com.github.peholmst.soapsamples.server.Contact;
+import com.github.peholmst.soapsamples.server.Gender;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
 
 public class ClientUI extends UI {
@@ -73,10 +84,45 @@ public class ClientUI extends UI {
         delete.setEnabled(hasSelection);
     }
 
+    public static class ContactWindow extends Window {
+
+        private final TextField firstName;
+        private final TextField lastName;
+        private final ComboBox gender;
+        private final BeanFieldGroup<Contact> binder;
+
+        public ContactWindow(Contact contact, SaveHandler saveHandler) {
+            binder = new BeanFieldGroup<>(Contact.class);
+            
+            setCaption("Contact details");
+            final FormLayout form = new FormLayout();
+            form.addComponent(firstName = new TextField("First name"));
+            form.addComponent(lastName = new TextField("Last name"));
+            form.addComponent(gender = new ComboBox("Gender", Arrays.asList(Gender.values())));
+            form.addComponent(new Button("Save", (event) -> {
+                try {
+                    binder.commit();
+                    saveHandler.save(binder.getItemDataSource().getBean());
+                } catch (FieldGroup.CommitException ex) {
+                    Notification.show("Could not commit form");
+                }
+            }));
+
+            binder.bindMemberFields(this);
+            binder.setItemDataSource(contact);
+            setContent(form);
+        }
+
+        @FunctionalInterface
+        public interface SaveHandler {
+            void save(Contact contact);
+        }
+    }
+
     private void refresh(ClickEvent evt) {
     }
 
-    private void create(ClickEvent evt) {
+    private void create(ClickEvent evt) {        
     }
 
     private void edit(ClickEvent evt) {
